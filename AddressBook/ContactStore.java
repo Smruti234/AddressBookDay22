8df8bbf4449a9ba7f66acf3bc11231692262baba
Smruti <lenkasmruti123@gmail.com>
2021-11-07 22:17:25 +0530

    Added: Uc11_AddressBook

--- AddressBook/ContactStore.java
+++ AddressBook/ContactStore.java
@@ -112,26 +112,9 @@ public class ContactStore extends AddressBook {
         System.out.println("Enter state name");
         Scanner scanner = new Scanner(System.in);
         String state = scanner.nextLine();
-        System.out.println("Enter city name");
-        String city = scanner.nextLine();
-        long count1 = 0;
-        long count2 = 0;
-        System.out.println("Persons with State name :" + state);
-        for (int i = 1; i <= dictionary.size(); i++) {
-            List<ContactStore> personWithState = dictionary.get(i).stream().filter(s -> s.getState().equalsIgnoreCase(state)).collect(Collectors.toList());
-            System.out.println(personWithState);
-            long count = dictionary.get(i).stream().filter(s -> s.getState().equalsIgnoreCase(state)).count();
-            count1 = count1 + count;
-        }
-        System.out.println(count1);
-        System.out.println("No.of Persons in State " + state + " are " + count1);
-        System.out.println("Persons with City name :" + city);
         for (int i = 1; i <= dictionary.size(); i++){
-            List<ContactStore> personWithCity = dictionary.get(i).stream().filter(c -> c.getCity().equalsIgnoreCase(city)).collect(Collectors.toList());
-            System.out.println(personWithCity);
-            long count = dictionary.get(i).stream().filter(c -> c.getCity().equalsIgnoreCase(city)).count();
-            count2 = count2 + count;
+            List<ContactStore> person = dictionary.get(i).stream().filter(s -> s.getState().equalsIgnoreCase(state)).collect(Collectors.toList());
+            System.out.println(person);
         }
-        System.out.println("No.of persons in City " + city + " are " + count2);
     }
 }
