57836c2c947a1a0ffe97d3a49f99c4144232561e
Smruti <lenkasmruti123@gmail.com>
2021-11-07 18:01:42 +0530

    Added:Uc2_AddressBook

--- AddressBook/AddressBookMain.java
+++ AddressBook/AddressBookMain.java
@@ -1,18 +1,62 @@
 package com.AddressBook;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBookMain {
+    public static String userInput() {
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        return st;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+        ArrayList<String> addressList = new ArrayList<>();
 
-        Person_Details person = new Person_Details();
-        person.FirstName("Smruti");
-        person.LastName("Lenka");
-        person.Email("lenkasmruti123@gmail.com");
-        person.State("ODISHA");
-        person.Address("Artung");
-        person.PhoneNumber("7504614661");
-        person.Zip(756046);
-        person.City("bhadrak");
-        System.out.println(person.toString());
+        Person_Details personOne = new Person_Details();
+        System.out.println("Please Enter the Name :");
+        personOne.setFirstName(userInput());
+        System.out.println("Please Enter Last Name : ");
+        personOne.setLastName(userInput());
+        System.out.println("Please Enter Address : ");
+        personOne.setAddress(userInput());
+        System.out.println("Please Enter City : ");
+        personOne.setCity(userInput());
+        System.out.println("Please Enter State : ");
+        personOne.setState(userInput());
+        System.out.println("Please Enter Zip Code : ");
+        personOne.setZipCode(userInput());
+        System.out.println("Please Enter Phone Number : ");
+        personOne.setPhoneNumber(userInput());
+        System.out.println("Please Enter Email : ");
+        personOne.setEmail(userInput());
+        System.out.println(personOne);
+
+        System.out.println("Enter the Second Person Details");
+
+        Person_Details personTwo = new Person_Details();
+        System.out.println("Please Enter the Name :");
+        personTwo.setFirstName(userInput());
+        System.out.println("Please Enter Last Name : ");
+        personTwo.setLastName(userInput());
+        System.out.println("Please Enter Address : ");
+        personTwo.setAddress(userInput());
+        System.out.println("Please Enter City : ");
+        personTwo.setCity(userInput());
+        System.out.println("Please Enter State : ");
+        personTwo.setState(userInput());
+        System.out.println("Please Enter Zip Code : ");
+        personTwo.setZipCode(userInput());
+        System.out.println("Please Enter Phone Number : ");
+        personTwo.setPhoneNumber(userInput());
+        System.out.println("Please Enter Email : ");
+        personTwo.setEmail(userInput());
+        System.out.println(personTwo);
+
     }
 }
+
+
+
+
+
--- AddressBook/Person_Details.java
+++ AddressBook/Person_Details.java
@@ -6,55 +6,78 @@ public class Person_Details {
     private String address;
     private String city;
     private String state;
-    private int zip;
+    private String zipCode;
     private String phoneNumber;
     private String email;
 
-    public void FirstName(String firstName) {
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    public void LastName(String lastName) {
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    public void Address(String address) {
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
         this.address = address;
     }
 
-    public void City(String city) {
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
         this.city = city;
     }
 
-    public void State(String state) {
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
         this.state = state;
     }
 
-    public void Zip(int zip) {
-        this.zip = zip;
+    public String getZipCode() {
+        return zipCode;
     }
 
-    public void PhoneNumber(String phoneNumber) {
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
-    public void Email(String email) {
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
 
     @Override
     public String toString() {
-        return "Person_Details{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", address='" + address + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", zip=" + zip +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", email='" + email + '\'' +
-                '}';
+        return "PersonDetails [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city="
+                + city + ", state=" + state + ", zipCode=" + zipCode + ", phoneNumber=" + phoneNumber + ", email="
+                + email + "]";
     }
 }
-
-
